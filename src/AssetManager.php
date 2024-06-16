bfb8e9329c7e7c67b80e79544ab4bf91c732d1fb
Martin <hello@northrook.com>
2024-06-16 06:31:20 +0100

    mend

--- src/AssetManager.php
+++ src/AssetManager.php
@@ -3,16 +3,88 @@
 namespace Northrook;
 
 
+use Northrook\AssetManager\Asset;
+use Northrook\Cache\ManifestCache;
 use Northrook\Core\Trait\SingletonClass;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 
+/**
+ * @template PathString of string
+ * @template AssetArray of array
+ */
 final class AssetManager
 {
     use SingletonClass;
 
-    public function __construct()
-    {
+    /** @var array<string, AssetArray> */
+    private array $enqueued = [
+        'stylesheet' => [],
+        'script'     => [],
+        'link'       => [],
+    ];
+    /** @var string<PathString> */
+    public readonly string $publicRoot;
+    /** @var string<PathString> */
+    public readonly string $publicAssets;
+
+    public function __construct(
+        string                            $publicRoot,
+        string                            $publicAssets,
+        private readonly AdapterInterface $cache,
+        private readonly ManifestCache    $manifest,
+        private readonly ?LoggerInterface $logger = null,
+    ) {
         $this->instantiationCheck();
 
+        $this->publicRoot   = normalizeRealPath( $publicRoot );
+        $this->publicAssets = normalizeRealPath( $publicAssets );
         AssetManager::$instance = $this;
+    }
+
+    public function addStylesheet(
+        string $href,
+        array  $attributes = [],
+        ?bool  $inline = null,
+    ) : AssetManager{
+        $this->enqueued[ 'stylesheet' ][] = [
+            'source' => $href,
+            'attributes' => $attributes,
+            'inline' => $inline,
+        ];
+        return $this;
+    }
+
+    public function addScript(
+        string $src,
+        array  $attributes = [],
+        ?bool  $inline = null,
+    ) : AssetManager{
+        $this->enqueued[ 'script' ][] = [
+            'source' => $src,
+            'attributes' => $attributes,
+            'inline' => $inline,
+        ];
+        return $this;
+    }
+
+    public function addLink(
+        string $src,
+        array  $attributes = [],
+    ) : AssetManager{
+        $this->enqueued[ 'link' ][] = [
+            'source' => $src,
+            'attributes' => $attributes,
+        ];
+        return $this;
+    }
+
+    /**
+     * Get the {@see AssetManager} cache adapter.
+     *
+     * @return AdapterInterface
+     */
+    public static function cache() : AdapterInterface {
+        return AssetManager::getInstance()->cache;
     }
 }
---
+++ src/AssetManager/StyleAsset.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Northrook\AssetManager;
+
+class StyleAsset
+{
+
+}
