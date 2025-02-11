edfb61b7aea266f88b6bc8fa0dac314831744b63
Martin <hello@northrook.com>
2025-02-11 09:54:55 +0000

    mend

--- src/AssetManager/Compiler/AssetManagerConfigurationPass.php
+++ src/AssetManager/Compiler/AssetManagerConfigurationPass.php
@@ -2,13 +2,30 @@
 
 namespace Core\AssetManager\Compiler;
 
+use Core\AssetManager\AssetConfig;
 use Core\Symfony\DependencyInjection\CompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class AssetManagerConfigurationPass extends CompilerPass
 {
+    private AssetConfig $config;
+
     public function compile( ContainerBuilder $container ) : void
     {
-        dump( $this->parameterBag->all() );
+        $this->config = new AssetConfig();
+
+        $this->parseConfigurationFiles();
+    }
+
+    private function parseConfigurationFiles() : void
+    {
+        foreach ( [
+            $this->getParameterPath( 'dir.core.config', '/assets.php' ),
+            $this->getParameterPath( 'dir.config', '/assets.php' ),
+        ] as $config ) {
+            if ( \file_exists( $config ) ) {
+                ( require $config )( $this->config );
+            }
+        }
     }
 }
