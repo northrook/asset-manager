f6395e2730e8dcba39843cb3650216fb86047461
Martin <hello@northrook.com>
2025-02-20 11:38:32 +0000

    mend

--- src/AssetManager/Compiler/AssetAttribute.php
+++ src/AssetManager/Compiler/AssetAttribute.php
@@ -4,22 +4,39 @@ declare(strict_types=1);
 
 namespace Core\AssetManager\Compiler;
 
-use Attribute;
 use Core\Asset\Type;
 use Core\Symfony\DependencyInjection\Autodiscover;
+use InvalidArgumentException;
 
 /**
- * @internal
- * @abstract-attribute
+ * If provided a `name`, it will match against registered assets by name.
+ * If provided a `type`, it will match against all registered assets of that type.
+ *
+ * Asset names must resolve to a valid asset and type.
  */
-abstract class AssetAttribute extends Autodiscover
+final class AssetAttribute extends Autodiscover
 {
+    /** @var ?string `type.name` */
+    public ?string $name;
+
+    public Type $type;
+
     public function __construct(
-        public readonly Type $type,
+        ?string $name = null,
+        ?Type   $type = null,
     ) {
+        if ( ! ( $name || $type ) ) {
+            throw new InvalidArgumentException(
+                'Either a name or a type must be provided.',
+            );
+        }
+
+        $this->name = $name;
+        $this->type = $type ?? Type::from( $name, true );
+
         parent::__construct(
             tag      : [
-                'core.asset.discovery',
+                'asset.service_locator',
                 'monolog.logger' => ['channel' => 'assets'],
             ],
             lazy     : false,
